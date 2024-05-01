bca4216531527f080f70c89a3f636aecbe0361c9
Dom <domenicomandica2000@gmail.com>
2024-05-01 22:42:45 +0930

    bring back ai manager

---
+++ Assets/Scripts/Playable/AIManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using SabberStoneBasicAI.Nodes;
+using SabberStoneBasicAI.Score;
+using SabberStoneCore.Enums;
+using SabberStoneCore.Model;
+using SabberStoneCore.Tasks.PlayerTasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    public static AIManager Instance = null;
+    private Game game;
+    private IScore scoring;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            scoring = new MidRangeScore();
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Init(Game game)
+    {
+        this.game = game;
+    }
+
+
+    public void PlayTurn()
+    {
+        if (game != null && game.State == State.RUNNING && game.CurrentPlayer == game.Player2)
+        {
+            List<OptionNode> solutions = OptionNode.GetSolutions(game, game.Player2.Id, scoring, 40, 40);
+            var solution = new List<PlayerTask>();
+            solutions.OrderByDescending(p => p.Score).First().PlayerTasks(ref solution);
+            foreach (PlayerTask task in solution)
+            {
+
+                Debug.Log(task.FullPrint());
+                game.Process(task);
+
+                if (game.CurrentPlayer.Choice != null)
+                    break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        PlayTurn();
+    }
+}
